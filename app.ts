b2caa7d0e99e76c4d3fa5f96d3678df7d10303e6
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-08-17 14:54:20 -0400

    Completed TS union and literal types

--- app.ts
+++ app.ts
@@ -1,47 +1,30 @@
-// const person: {
-//   name: string;
-//   age: number;
-//   hobbies: string[]; // string array type
-//   role: [number, string];
-// } = {
-//   name: 'Laura',
-//   age: 22,
-//   hobbies: ['Arts', 'Programming'],
-//   role: [2, 'author'] // Tuple type Array with fixed type and length
-// };
+// Union types & Literal types
 
-// Better use ENUM instead (Added by TS)
-// const ADMIN = 0;
-// const READ_ONLY = 1;
-// const AUTHOR = 2;
-
-enum Role { ADMIN, READ_ONLY, AUTHOR };
-// enum Role { ADMIN = 5, READ_ONLY = 'read only' } // Can change the default starting from 0
-
-const person = {
-  name: 'Laura',
-  age: 22,
-  hobbies: ['Arts', 'Programming'],
-  role: Role.ADMIN
-};
-
-// TUPLE (Added by TS)
-// person.role.push('admin'); // push exception that is allowed on tuple
-// person.role[1] = 10; // !!! ERROR cannot be number need to be string !!!
-// person.role = [0, 'admin', 'user']; // !!! ERROR length can only be 2 !!!
-
-// ARRAY
-// let favoriteActivities: any[]; // mix array type
-// favoriteActivities = ['apple', 1];
-
-console.log(person.name);
-
-for (const hobby of person.hobbies) {
-  // TS let us use any method on strings bcz hobby is set as string type
-  console.log(hobby.toUpperCase());
-  // console.log(hobby.map()); // !!! ERROR !!!
+function combine(
+  input1: number | string,
+  input2: number | string,
+  resultConversion: 'as-number' | 'as-text' // union + literal
+) {
+  let result;
+  // result = input1 + input2 // !!! ERROR bcz TS does not check what types of union it is and some types cannot be added together
+  if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+    result = +input1 + +input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
+  // if (resultConversion === 'as-number') {
+  //   return +result;
+  // } else {
+  //   return result.toString();
+  // }
 }
 
-if (person.role == Role.READ_ONLY) {
-  console.log('is read only');
-}
+const combinedAges = combine(30, 26, 'as-number');
+console.log(combinedAges);
+
+const combinedStringAges = combine('30', '26', 'as-number');
+console.log(combinedStringAges);
+
+const combinedNames = combine('Max', 'Anna', 'as-text');
+console.log(combinedNames);
